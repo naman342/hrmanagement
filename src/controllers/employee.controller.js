7d951b8b1971c80c65ede57da7cae12866a98440
Bharat Khetan <bharatkhetan@Bharats-MacBook-Air.local>
2026-05-28 21:08:40 +0530

    [MA-1234] code for refactor  for validation cretae employee API

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,14 +1,41 @@
+const {
+    employeeSchema
+} = require('../validators/employee.validator');
+
 async function createEmployee(req, res) {
 
-    const employee = {
-        id: Date.now(),
-        fullName: req.body.fullName,
-        jobTitle: req.body.jobTitle,
-        country: req.body.country,
-        salary: req.body.salary
-    };
+    try {
 
-    res.status(201).json(employee);
+        const { error } =
+            employeeSchema.validate(req.body);
+
+        if (error) {
+            let message =
+                error.details[0].message;
+
+            return res.status(400).json({
+                message
+            });
+
+        }
+
+        const employee = {
+            id: Date.now(),
+            ...req.body
+        };
+
+        res.status(201).json(employee);
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+
+    }
+
 }
 
 module.exports = {
